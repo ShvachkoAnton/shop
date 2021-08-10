6f197aece9b15cb2d22e807d31204b239fdc05cb
zeratul314@gmail.com <zeratul314@gmail.com>
2021-08-10 17:23:37 +0300

    glavnaya stranica

--- myshop/settings.py
+++ myshop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'myshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -133,8 +134,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[str(BASE_DIR.joinpath('static'))]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 # Create your models here.
 class Category(models.Model):
     slug=models.SlugField(max_length=200, db_index=True)
@@ -7,6 +8,10 @@ class Category(models.Model):
         ordering=('name',)
         verbose_name='category'
         verbose_name_plural='categories'
+    def get_absolute_url(self):
+        return reverse("product_list_by_category", args=[self.slug])
+    
+
 
 class Product(models.Model):
     category=models.ForeignKey(Category, related_name='products',
@@ -25,3 +30,6 @@ class Product(models.Model):
         index_together=(('id','slug'))
     def __str__(self):
         return self.name
+    def get_absolute_url(self):
+        return reverse("product_detail", args=[self.id, self.slug])
+    
